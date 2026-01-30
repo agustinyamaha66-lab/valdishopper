aa9e69ba98038dd749c05050cccdc3687d67f500
Agust <agustinyamaha66@gmail.com>
2026-01-30 12:12:17 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// IMPORTANTE: ¿Está esta línea?
-import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* IMPORTANTE: ¿Está App dentro de AuthProvider? */}
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
+    <App />
 )
