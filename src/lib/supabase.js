2c7933a7a7ced08acc82498768efd3760f9826c3
Agust <agustinyamaha66@gmail.com>
2026-01-30 14:03:25 -0300

    clave

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,6 +5,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://ceqqxyszrkbuzvlqnvfp.supabase.co'
 
 // LA CLAVE CORRECTA DEBE EMPEZAR ASÍ (NO ES sb_publishable...)
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlcXF4eXN6cmtidXp2bHFudmZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNzUyNjIsImV4cCI6MjA4NDc1MTI2Mn0.xuv8LHS8HIq37IgWlj87cknnMQo2r3XBpnmTtC_Pu-U'
+const supabaseKey = 'sb_publishable_62b3jcxbXz5JMSGrBP194w_X3dSNZiu'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
